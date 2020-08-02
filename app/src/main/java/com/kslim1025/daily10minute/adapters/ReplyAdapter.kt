51937b54db96ba4b2b8a25487ba0c51d6502705a
choihyunill <kslim1025@naver.com>
2020-08-02 11:17:09 +0900

    좋아요 기능 추가

---
+++ app/src/main/java/com/kslim1025/daily10minute/adapters/ReplyAdapter.kt
@@ -0,0 +1,102 @@
+package com.kslim1025.daily10minute.adapters
+
+import android.content.Context
+import android.graphics.Color
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import com.bumptech.glide.Glide
+import com.kslim1025.daily10minute.R
+import com.kslim1025.daily10minute.datas.Reply
+import com.kslim1025.daily10minute.utils.ServerUtil
+import org.json.JSONObject
+
+
+class ReplyAdapter(
+    val mContext:Context,
+    resId:Int,
+    val mList:List<Reply>) : ArrayAdapter<Reply>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        var tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.reply_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val writerNickNameTxt = row.findViewById<TextView>(R.id.writerNickNameTxt)
+        val contentTxt = row.findViewById<TextView>(R.id.contentTxt)
+        val likeBtn = row.findViewById<Button>(R.id.likeBtn)
+        val likeCountTxt = row.findViewById<TextView>(R.id.likeCountTxt)
+
+        val data = mList[position]
+
+        writerNickNameTxt.text = data.writer.nickName
+        contentTxt.text = data.content
+
+//        좋아요 갯수 처리 -> 0개면 숨김, 그 이상이면 보여주고 갯수 반영
+        if (data.likeCount == 0) {
+            likeCountTxt.visibility = View.GONE
+        }
+        else {
+            likeCountTxt.visibility = View.VISIBLE
+            likeCountTxt.text = "좋아요 ${data.likeCount}개"
+        }
+
+//        내 좋아요 여부에 따른 처리
+//        myLike가 true : 빨간 테두리 + 빨간 글씨 (naverRed) + 좋아요 취소
+//        반대 : 회색 테두리 + 검정 글씨 + 좋아요
+
+        if (data.myLike) {
+//            테두리 처리 => xml로 그려둔 drawable 활용
+            likeBtn.setBackgroundResource(R.drawable.red_border_box)
+
+//            빨간 글씨 => res > colors.xml 에 있는 항목 (naverRed) 가져오기
+            likeBtn.setTextColor(mContext.resources.getColor(R.color.naverRed))
+
+//            좋아요 취소로 문구 변경
+            likeBtn.text = "좋아요 취소"
+
+        }
+        else {
+            likeBtn.setBackgroundResource(R.drawable.gray_border_box)
+//            검정 글씨 => 기본색에서 BLACK 지원 (기본 색 사용법 예시)
+            likeBtn.setTextColor(Color.BLACK)
+            likeBtn.text = "좋아요"
+        }
+
+        likeBtn.setOnClickListener {
+
+            ServerUtil.postRequestLikeReply(mContext, data.id, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(json: JSONObject) {
+
+//                    서버가 주는 메세지를 토스트로 출력
+
+                    val message = json.getString("message")
+
+                    val uiHandler = Handler(Looper.getMainLooper())
+
+                    uiHandler.post {
+                        Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+
+//                        어댑터 새로고침
+                        notifyDataSetChanged()
+                    }
+
+                }
+
+            })
+
+        }
+
+        return row
+    }
+
+
+}
--- app/src/main/java/com/kslim1025/daily10minute/datas/Reply.kt
+++ app/src/main/java/com/kslim1025/daily10minute/datas/Reply.kt
@@ -1,7 +1,30 @@
 package com.kslim1025.daily10minute.datas
 
+import org.json.JSONObject
+
 class Reply {
+
+    var id = 0
     var content = ""
     lateinit var writer : User
     var likeCount = 0
+    var myLike = false
+
+    companion object {
+
+        fun getReplyFromJson(json : JSONObject) : Reply {
+            val r = Reply()
+
+            r.id = json.getInt("id")
+            r.content = json.getString("content")
+            r.likeCount = json.getInt("like_count")
+            r.myLike = json.getBoolean("my_like")
+
+//            user JSONObject 추출 => User 클래스로 변환 => r.writer에 대입
+            r.writer = User.getUserFromJson(json.getJSONObject("user"))
+
+            return r
+        }
+
+    }
 }
